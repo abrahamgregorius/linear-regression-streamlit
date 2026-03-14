aae553b081cffc87d6aaad77a7ddd92627d90623
abrahamgregorius <athioii6@gmail.com>
2026-03-14 14:30:43 +0700

    fix: dir

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-df = pd.read_csv("data.csv")
+df = pd.read_csv("./data.csv")
 
-with open("model.pkl", "rb") as f:
+with open("./model.pkl", "rb") as f:
     model = pickle.load(f)
     
 st.title("Linear Regression Streamlit")
